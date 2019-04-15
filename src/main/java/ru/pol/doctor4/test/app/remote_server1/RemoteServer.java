a1ec67af83f213a87a4eba53a8e10a3d07149fae
PolBadWolf <gm.gameoff@gmail.com>
2019-04-15 22:52:36 +0500

    replace interface fix metod

--- src/main/java/ru/pol/doctor4/test/app/remote_server1/RemoteServer.java
+++ src/main/java/ru/pol/doctor4/test/app/remote_server1/RemoteServer.java
@@ -1,15 +1,41 @@
 package ru.pol.doctor4.test.app.remote_server1;
 
-import ru.pol.doctor4.test.interfaces.MyException;
-import ru.pol.doctor4.test.interfaces.RemoteIntrface1;
+import ru.pol.doctor4.test.interfaces.RemoteInterface1;
 
-class RemoteServer implements RemoteIntrface1 {
+import java.net.*;
+import java.rmi.*;
+import java.rmi.server.*;
 
-    public String retString(String s) throws MyException {
-        throw new MyException();
+class RemoteServer extends UnicastRemoteObject implements RemoteInterface1 {
+
+    protected RemoteServer() throws RemoteException {
     }
 
-    public static void main(String[] args) {
-        new RemoteServer().retString("");
+    public String datString(String s) throws RemoteException {
+        System.out.println("Клиент: \"" + s + "\"");
+        return null;
+    }
+    private String revers(String s) {
+        int lenght = s.length();
+        if (lenght <= 0) return "";
+        int indMax = lenght - 1;
+        String ss = "";
+        for (int i=0; i<lenght; i++) {
+            ss += s.charAt(indMax - i);
+        }
+        return ss;
+    }
+    public static void main(String[] args) throws RemoteException {
+        new RemoteServer().go();
+    }
+    private void go() {
+        try {
+            RemoteInterface1 service1 = new RemoteServer();
+            Naming.rebind("Interface1", service1);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
     }
 }
