7ed61a9b891160e044ac77e149716c2692230d33
PolBadWolf <gm.gameoff@gmail.com>
2019-04-15 20:33:07 +0500

    set myException

--- src/main/java/ru/pol/doctor4/test/app/remote_server1/RemoteServer.java
+++ src/main/java/ru/pol/doctor4/test/app/remote_server1/RemoteServer.java
@@ -1,7 +1,15 @@
 package ru.pol.doctor4.test.app.remote_server1;
 
-class  Main {
-    public static void main(String[] args) {
+import ru.pol.doctor4.test.interfaces.MyException;
+import ru.pol.doctor4.test.interfaces.RemoteIntrface1;
 
+class RemoteServer implements RemoteIntrface1 {
+
+    public String retString(String s) throws MyException {
+        throw new MyException();
+    }
+
+    public static void main(String[] args) {
+        new RemoteServer().retString("");
     }
 }
