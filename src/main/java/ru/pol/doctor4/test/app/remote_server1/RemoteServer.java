decb246f3ede08215212a55d09c2f7556b0ce1de
PolBadWolf <gm.gameoff@gmail.com>
2019-04-16 14:16:15 +0500

    upd for work pc

--- src/main/java/ru/pol/doctor4/test/app/remote_server1/RemoteServer.java
+++ src/main/java/ru/pol/doctor4/test/app/remote_server1/RemoteServer.java
@@ -1,36 +1,65 @@
 package ru.pol.doctor4.test.app.remote_server1;
 
-import ru.pol.doctor4.test.interfaces.RemoteInterface1;
+import ru.polbadwolf.test.interfaces.remoteRMI.RemoteInterface;
 
 import java.net.*;
 import java.rmi.*;
 import java.rmi.server.*;
+import java.time.LocalDateTime;
+import java.util.Timer;
 
-class RemoteServer extends UnicastRemoteObject implements RemoteInterface1 {
+class RemoteServer extends UnicastRemoteObject implements RemoteInterface {
+    BlockData datGet = new BlockData();
+    BlockData datPut = new BlockData();
+    public String retStatus() {
 
-    protected RemoteServer() throws RemoteException {
+        return LocalDateTime.now().toString();
     }
 
-    public String datString(String s) throws RemoteException {
-        System.out.println("Клиент: \"" + s + "\"");
-        return null;
+    public void putMessage(String s) {
+        System.out.println(s);
     }
-    private String revers(String s) {
-        int lenght = s.length();
-        if (lenght <= 0) return "";
-        int indMax = lenght - 1;
-        String ss = "";
-        for (int i=0; i<lenght; i++) {
-            ss += s.charAt(indMax - i);
-        }
-        return ss;
+
+    public void setBlockData(BlockData blockData) {
+        datPut = blockData;
     }
+
+    public BlockData getBlockData() {
+        return datGet;
+    }
+
+    public RemoteServer() throws RemoteException {
+    }
+
+    /*
+            protected RemoteServer() throws RemoteException {
+            }
+
+            public String datString(String s) throws RemoteException {
+                System.out.println("Клиент: \"" + s + "\"");
+                return null;
+            }
+            private String revers(String s) {
+                int lenght = s.length();
+                if (lenght <= 0) return "";
+                int indMax = lenght - 1;
+                String ss = "";
+                for (int i=0; i<lenght; i++) {
+                    ss += s.charAt(indMax - i);
+                }
+                return ss;
+            }
+
+         */
     public static void main(String[] args) throws RemoteException {
         new RemoteServer().go();
     }
+
     private void go() {
+        datGet.name = new String[] {"Василий" };
+        datGet.content = new int[] {345};
         try {
-            RemoteInterface1 service1 = new RemoteServer();
+            RemoteInterface service1 = new RemoteServer();
             Naming.rebind("Interface1", service1);
         } catch (RemoteException e) {
             e.printStackTrace();
@@ -38,4 +67,5 @@ class RemoteServer extends UnicastRemoteObject implements RemoteInterface1 {
             e.printStackTrace();
         }
     }
+
 }
